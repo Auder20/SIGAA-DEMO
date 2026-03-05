4f91e6c7da48fd57077f79ff463ca29a9f7995a4
Auder20 <audergonzalez@gmail.com>
2026-03-05 17:36:59 -0500

    cambios en yaml y otras correcciones

--- sigaa/settings.py
+++ sigaa/settings.py
@@ -103,8 +103,6 @@ else:
             'PORT': DB_PORT,
             "CONN_MAX_AGE": 300,
             "OPTIONS": {
-                "init_command": "SET sql_mode='STRICT_TRANS_TABLES'",
-                "charset": "utf8mb4",
                 "connect_timeout": 60,
             },
         }
