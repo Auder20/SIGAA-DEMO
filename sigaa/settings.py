be370d6660a1a600454bd90c2022b65b3a680c38
Auder20 <audergonzalez@gmail.com>
2026-03-05 16:59:30 -0500

    cambios en settings

--- sigaa/settings.py
+++ sigaa/settings.py
@@ -143,15 +143,13 @@ LOGIN_REDIRECT_URL = 'home'
 LOGOUT_REDIRECT_URL = 'users:login'
 
 # Configuración de correo electrónico (para restablecimiento de contraseña)
-EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'  # Para desarrollo
-# Descomenta y configura para producción:
-# EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
-# EMAIL_HOST = 'smtp.tudominio.com'
-# EMAIL_PORT = 587
-# EMAIL_USE_TLS = True
-# EMAIL_HOST_USER = 'tu@email.com'
-# EMAIL_HOST_PASSWORD = 'tu_contraseña'
-# DEFAULT_FROM_EMAIL = 'SIGAA <noreply@tudominio.com>'
+EMAIL_BACKEND = config('EMAIL_BACKEND', default='django.core.mail.backends.console.EmailBackend')
+EMAIL_HOST = config('EMAIL_HOST', default='')
+EMAIL_PORT = config('EMAIL_PORT', default=587, cast=int)
+EMAIL_USE_TLS = config('EMAIL_USE_TLS', default=True, cast=bool)
+EMAIL_HOST_USER = config('EMAIL_HOST_USER', default='')
+EMAIL_HOST_PASSWORD = config('EMAIL_HOST_PASSWORD', default='')
+DEFAULT_FROM_EMAIL = config('DEFAULT_FROM_EMAIL', default=f'SIGAA <{config("EMAIL_HOST_USER", default="noreply@localhost")}>')
 
 # Configuración de sesión
 SESSION_COOKIE_AGE = 1209600  # 2 semanas en segundos
