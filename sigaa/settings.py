953e1400cf0b37352ca59cb022b7af078a6b268f
Auder20 <audergonzalez@gmail.com>
2026-03-05 18:03:09 -0500

    cambios en settings para conexion con base de datos

--- sigaa/settings.py
+++ sigaa/settings.py
@@ -104,6 +104,8 @@ else:
             "CONN_MAX_AGE": 300,
             "OPTIONS": {
                 "connect_timeout": 60,
+                "init_command": "SET sql_mode='STRICT_TRANS_TABLES'",
+                "charset": "utf8mb4",
             },
         }
     }
